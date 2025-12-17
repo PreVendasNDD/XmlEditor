f03de919fdd173ba332c488ab6b25ddab5a92498
PreVendasNDD <joao.pinheiro@ndd.tech>
2025-12-17 10:32:24 -0300

    Delete web directory

--- web/script.js
+++
@@ -1,7 +0,0 @@
-function gerar() {
-  alert(
-    "Geração iniciada.\n\n" +
-    "Acesse a aba ACTIONS do repositório.\n" +
-    "Quando o workflow finalizar, faça o download dos XMLs em ARTIFACTS."
-  );
-}
