648a459dbf1c59941fd1d9f53fc01ae2e029a908
PreVendasNDD <joao.pinheiro@ndd.tech>
2025-12-17 09:16:07 -0300

    Create script.js

---
+++ web/script.js
@@ -0,0 +1,7 @@
+function gerar() {
+  alert(
+    "Geração iniciada.\n\n" +
+    "Acesse a aba ACTIONS do repositório.\n" +
+    "Quando o workflow finalizar, faça o download dos XMLs em ARTIFACTS."
+  );
+}
